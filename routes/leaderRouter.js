6eb5b6e92cfe3aa8533c3b38a91797c372ebb85a
cesar4rroyo <cesardavid_09@hotmail.com>
2020-08-09 18:46:03 -0500

    task04

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -17,7 +17,7 @@ leaderRouter.route('/')
         }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post(cors.corsWithOptions,, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Leaders.create(req.body)
             .then((leader) => {
                 console.log('Leader Created ', leader);
@@ -27,7 +27,7 @@ leaderRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .put(cors.corsWithOptions,, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /leaderes');
     })
